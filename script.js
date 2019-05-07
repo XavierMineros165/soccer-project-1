d44b0d6a2b64338f2b21a63fea9791c57694365a
XavierMineros165 <47435059+XavierMineros165@users.noreply.github.com>
2019-05-07 15:16:32 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".mejor").click(function() {
+    $(".messi").hide();
+});
+$(".team").click(function() {
+    $(".barca").fadeToggle();
+});
+$(".estadio").click(function() {
+    $(".club").slideToggle();
+});
+
+
+
+
+
+
+
+
+
+$("#change").click(function() {
+    $("body").css("background-color", "purple");
+});
+$("#black").click(function() {
+    $("body").css("background-color", "black");
+});
